aadd98ea0507809fef485bca6d8e022ecbb475df
DidierShemaGatete <didiershemagate01@gmail.com>
2023-06-08 10:32:05 +0300

    solution for Hashtag generator kata

---
+++ HashTagGenerator.js
@@ -0,0 +1,16 @@
+
+function generateHashtag(str) {
+    if (str.trim() === "") {
+      return false;
+    }
+  
+    const words = str.trim().split(/\s+/);
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    const hashtag = "#" + capitalizedWords.join("");
+  
+    if (hashtag.length > 140) {
+      return false;
+    }
+  
+    return hashtag;
+  }
